093d77ff2c7c6827a46d21ef8231eed9980499a3
hrutik-codes <hrutikjagdale@gmail.com>
2026-03-08 00:01:08 +0530

    fix: complete CRA to Vite migration with initial component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {Component} from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
+import {Route, Switch, Redirect, BrowserRouter} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
-import Home from './components/Home'
+/*import Home from './components/Home'
 import Products from './components/Products'
 import ProductItemDetails from './components/ProductItemDetails'
 import Cart from './components/Cart'
 import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
+import ProtectedRoute from './components/ProtectedRoute'*/
 import CartContext from './context/CartContext'
 
 import './App.css'
@@ -91,19 +91,21 @@ class App extends Component {
           decrementCartItemQuantity: this.decrementCartItemQuantity,
         }}
       >
-        <Switch>
-          <Route exact path="/login" component={LoginForm} />
-          <ProtectedRoute exact path="/" component={Home} />
-          <ProtectedRoute exact path="/products" component={Products} />
-          <ProtectedRoute
-            exact
-            path="/products/:id"
-            component={ProductItemDetails}
-          />
-          <ProtectedRoute exact path="/cart" component={Cart} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect to="not-found" />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            {/* <ProtectedRoute exact path="/" component={Home} /> */}
+            {/* <ProtectedRoute exact path="/products" component={Products} /> */}
+            {/* <ProtectedRoute
+              exact
+              path="/products/:id"
+              component={ProductItemDetails}
+            /> */}
+            {/* <ProtectedRoute exact path="/cart" component={Cart} /> */}
+            {/* <Route path="/not-found" component={NotFound} /> */}
+            {/* <Redirect to="not-found" /> */}
+          </Switch>
+        </BrowserRouter>
       </CartContext.Provider>
     )
   }
