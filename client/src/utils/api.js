147c8f658cb0f0a172c317edff0c76b0e039b0fa
hrutik-codes <hrutikjagdale@gmail.com>
2026-03-14 23:01:14 +0530

    fix: debug env var in production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie'
 
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+console.log('API BASE_URL:', BASE_URL)
 
 export const getToken = () => {
   return Cookies.get('jwt_token')
